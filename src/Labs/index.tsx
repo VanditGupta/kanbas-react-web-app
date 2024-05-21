3328340988953bc98bc6255c67c421375432e938
VanditGupta <vanditgupta22@gmail.com>
2024-05-21 18:27:25 -0400

    Added Section to the index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,7 +6,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
   return (
     <div id="wd-labs">
-      <h1>Vandit Gupta</h1>
+      <h1>Vandit Gupta Section 02</h1>
       <h1>Labs</h1>
             <TOC />
       <Routes>
