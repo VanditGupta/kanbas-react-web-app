45a0cd0d57bf4b69fdc01f694173320cb3abe854
VanditGupta <vanditgupta22@gmail.com>
2024-06-18 21:24:52 -0400

    Completed 2.6

---
+++ src/Labs/Lab3/AddPathParameters.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function AddPathParameters() {
+  const { a, b } = useParams<{ a: string; b: string }>();
+
+  const numA = parseInt(a ?? "3", 10);
+  const numB = parseInt(b ?? "4", 10);
+
+  return (
+    <div id="wd-add">
+      <hr />
+      <h4>Add Path Parameters</h4>
+      {numA} + {numB} = {numA + numB}
+    </div>
+  );
+}
