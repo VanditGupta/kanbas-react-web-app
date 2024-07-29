305c9d3cdc8dfdea4c6e6a29678411ea2701f602
VanditGupta <vanditgupta22@gmail.com>
2024-07-29 00:14:35 -0400

    Added loginID, Section, LA, TA to Users.json

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
 import * as client from "./client";
+
 export default function PeopleTable() {
   const [users, setUsers] = useState<any[]>([]);
+
   const fetchUsers = async () => {
     const users = await client.findAllUsers();
     setUsers(users);
   };
+
   useEffect(() => {
     fetchUsers();
   }, []);
+
   return (
     <div id="wd-people-table">
       <table className="table table-striped">
@@ -26,7 +30,7 @@ export default function PeopleTable() {
           {users.map((user: any) => (
             <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
-                <span className="wd-first-name">{user.firstName}</span>
+                <span className="wd-first-name">{user.firstName}</span>{" "}
                 <span className="wd-last-name">{user.lastName}</span>
               </td>
               <td className="wd-login-id">{user.loginId}</td>
